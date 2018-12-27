a018e06dd5b55adb2cacc7af67e7e5e1a5555a2c
Avinash Reddy <beavinash15@gmail.com>
2018-12-27 04:39:43 -0800

    remove operation complete.

--- LinkedList.playground/Contents.swift
+++ LinkedList.playground/Contents.swift
@@ -1,3 +1,136 @@
+import Foundation
+
+public class Node<Value> {
+    public var value: Value
+    public var next: Node?
+    
+    public init(value: Value, next: Node? = nil) {
+        self.value = value
+        self.next = next
+    }
+}
+
+extension Node: CustomStringConvertible {
+    public var description: String {
+        guard let next = next else {
+            return "\(value)"
+        }
+        return "\(value) -> " + String(describing: next)
+    }
+}
+
+public struct LinkedList<Value> {
+    public var head: Node<Value>?
+    public var tail: Node<Value>?
+    
+    public init() {
+        
+    }
+    
+    public var isEmpty: Bool {
+        return head == nil
+    }
+    
+    public mutating func push(_ value: Value) {
+        head = Node(value: value, next: head)
+        if tail == nil {
+            tail = head
+        }
+    }
+    
+    public mutating func append(_ value: Value) {
+        guard !isEmpty  else {
+            push(value)
+            return
+        }
+        
+        tail!.next = Node(value: value)
+        
+        tail = tail!.next
+    }
+    
+    public func node(at index: Int) -> Node<Value>? {
+        var currentNode = head
+        var currentIndex = 0
+        
+        while currentNode != nil && currentIndex < index {
+            currentNode = currentNode?.next
+            currentIndex += 1
+        }
+        
+        return currentNode
+    }
+    
+    @discardableResult
+    public mutating func insert(_ value: Value, after node: Node<Value>) -> Node<Value> {
+        
+        guard tail !== node else {
+            append(value)
+            return tail!
+        }
+        
+        node.next = Node(value: value, next: node.next)
+        
+        return node.next!
+    }
+    
+    @discardableResult
+    public mutating func pop() -> Value? {
+        defer {
+            head = head?.next
+            if isEmpty {
+                tail = nil
+            }
+        }
+        return head?.value
+    }
+    
+    @discardableResult
+    public mutating func removeLast() -> Value? {
+        guard let head = head else {
+            return nil
+        }
+        
+        guard head.next != nil else {
+            return pop()
+        }
+        
+        var prev = head
+        var current = head
+        
+        while let next = current.next {
+            prev = current
+            current = next
+        }
+        
+        prev.next = nil
+        tail = prev
+        return current.value
+    }
+    
+    @discardableResult
+    public mutating func remove(after node: Node<Value>) -> Value? {
+        defer {
+            if node.next === tail {
+                tail = node
+            }
+            node.next = node.next?.next
+        }
+        return node.next?.value
+    }
+}
+
+extension LinkedList: CustomStringConvertible {
+    public var description: String {
+        guard let head = head else {
+            return "Empty List"
+        }
+        return String(describing: head)
+    }
+}
+
+
+
 let node1 = Node(value: 1)
 let node2 = Node(value: 2)
 let node3 = Node(value: 3)
@@ -29,3 +162,18 @@ for _ in 1...4 {
     middleNode = list.insert(-1, after: middleNode)
 }
 print("After inserting: \(list)")
+
+var popList = LinkedList<Int>()
+popList.pop()
+popList.append(1)
+popList.append(2)
+popList.pop()
+
+
+var removeLast = LinkedList<Int>()
+removeLast.push(3)
+removeLast.push(2)
+removeLast.push(1)
+
+print("Before removing last node: \(list)")
+let removedValue = removeLast.removeLast()
